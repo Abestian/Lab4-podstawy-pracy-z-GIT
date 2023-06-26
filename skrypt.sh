0d05180a40121ee8acb91689e489baf4815b4713
Sebastian Kliś <klis.seb@gmail.com>
2023-06-26 21:54:16 +0200

    Dodanie flagi --help, która informuje o dostępnych komendach

--- skrypt.sh
+++ skrypt.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# ten kod sprawdza czy podano argumenty, jeśli nie podano to tworzy 30 plików
+# sprawdzanie, czy flaga --help została użyta
+if [ "$1" == "--help" ]; then
+    echo "Dostępne flagi to:"
+    echo "--date: wyświetla dzisiejszą datę"
+    echo "--logs [liczba]: tworzy określoną liczbę plików log; domyślnie tworzy 30 plików"
+    exit 0
+fi
 
+# sprawdzanie, czy flaga --date została użyta
 if [ "$1" == "--date" ]; then
     echo "Dzisiejsza data to: $(date)"
 fi
 
+# sprawdzanie, czy flaga --logs została użyta
 if [ "$1" == "--logs" ]; then
+    # ustawienie domyślnej liczby plików na 30, jeśli nie podano argumentu
     num_files=${2:-30}
 
     for i in $(seq 1 $num_files)
