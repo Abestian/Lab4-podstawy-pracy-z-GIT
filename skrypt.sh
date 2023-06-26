0fc956c8e029b7bbbd7ad7538ca75f7bec7afd10
Sebastian Kliś <klis.seb@gmail.com>
2023-06-26 21:43:37 +0200

    Dodanie do skrypt.sh możliwości użycia flagi --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# sprawdzanie, czy flaga --date została użyta
 if [ "$1" == "--date" ]; then
     echo "Dzisiejsza data to: $(date)"
+fi
+
+if [ "$1" == "--logs" ]; then
+    for i in {1..100}
+    do
+        touch log$i.txt
+        echo "Nazwa pliku: log$i.txt" > log$i.txt
+        echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
+        echo "Data utworzenia: $(date)" >> log$i.txt
+    done
 fi
