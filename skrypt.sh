e037031e4aed4977702181a913967a2b18e830e3
Sebastian Kliś <klis.seb@gmail.com>
2023-06-26 21:49:52 +0200

    Modyfikacja flagi --logs, od teraz przyjmuje po spacji argumenty, w przypadku braku argumentu tworzy 30 plików

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# ten kod sprawdza czy podano argumenty, jeśli nie podano to tworzy 30 plików
+
 if [ "$1" == "--date" ]; then
     echo "Dzisiejsza data to: $(date)"
 fi
 
 if [ "$1" == "--logs" ]; then
-    for i in {1..100}
+    num_files=${2:-30}
+
+    for i in $(seq 1 $num_files)
     do
         touch log$i.txt
         echo "Nazwa pliku: log$i.txt" > log$i.txt
