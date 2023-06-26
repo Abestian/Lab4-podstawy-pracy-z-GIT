b8a64afa6389091d83bec5809149c837e960d544
Sebastian Kliś <klis.seb@gmail.com>
2023-06-26 22:04:06 +0200

    Dodano możliwość użycia flag z pojedyńczym -(i tylko pierwszej liter)

--- skrypt.sh
+++ skrypt.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # sprawdzanie, czy flaga --help została użyta
-if [ "$1" == "--help" ]; then
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     echo "Dostępne flagi to:"
-    echo "--date: wyświetla dzisiejszą datę"
-    echo "--logs [liczba]: tworzy określoną liczbę plików log; domyślnie tworzy 30 plików"
+    echo "--help (-h): wyświetla dostępne flagi"
+    echo "--date (-d): wyświetla dzisiejszą datę"
+    echo "--logs (-l) [liczba]: tworzy określoną liczbę plików log; domyślnie tworzy 30 plików"
     exit 0
 fi
 
 # sprawdzanie, czy flaga --date została użyta
-if [ "$1" == "--date" ]; then
+if [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
     echo "Dzisiejsza data to: $(date)"
 fi
 
 # sprawdzanie, czy flaga --logs została użyta
-if [ "$1" == "--logs" ]; then
+if [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
     # ustawienie domyślnej liczby plików na 30, jeśli nie podano argumentu
     num_files=${2:-30}
 
